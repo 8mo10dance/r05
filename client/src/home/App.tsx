692a1c09e8e6cf8f7dbfc0aa29770d71d84503a3
寸田智也 <dexn@xeo.local>
2022-02-22 18:22:25 +0900

    CSSリセットする

--- client/src/home/App.tsx
+++ client/src/home/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
 
 const App: React.VFC<{}> = () => {
-  return <h1>hoge</h1>;
+  return (
+    <>
+      <CssBaseline />
+      <h1>hoge</h1>
+    </>
+  );
 };
 
 export default App;
