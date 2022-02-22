30e8629feee42eac92b6c36f12eae99883571e8b
寸田智也 <dexn@xeo.local>
2022-02-22 22:14:45 +0900

    committee-rails を設定する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,11 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.add_setting :committee_options
+  config.committee_options = {
+    schema_path: Rails.root.join('schema', 'schema.yml').to_s,
+    query_hash_key: 'rack.request.query_hash',
+    parse_response_by_content_type: false,
+  }
+  include Committee::Rails::Test::Methods
 end
