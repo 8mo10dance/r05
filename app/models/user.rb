13b3e403368b3f3c653957617a9355f1e00b6471
寸田智也 <dexn@xeo.local>
2022-03-13 17:59:44 +0900

    rails g sorcery:install

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+
   has_many :memos, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
